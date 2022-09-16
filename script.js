5be24ff11a3f1f613abb50f574caa2f017c1dbee
Gabi <pagameri@gmail.com>
2022-09-16 23:56:02 +0100

    Write code to play (needs to finish)
    Game does not end at game over or win at this stage yet.

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-
-// gameBoard // module
-
-// displayController // module
-
-// players // factory
-
 const Gameboard = (() => {
   const boardContainer = document.querySelector('.board-container');
   const cells = document.querySelectorAll('.cell');
@@ -32,8 +25,20 @@ const Gameboard = (() => {
 Gameboard.render();
 
 
-const displayController = (function() {
+
+const displayController = (() => {
+  const Player = (name, sign) => {
+    const getSign = () => sign;
+    const getName = () => name;
+    let board = ['', '', '', '', '', '', '', '', ''];
+  
+    return {getSign, getName, board};
+  }
+  
+  const player1 = Player('Gabi', 'X');
+  const player2 = Player('Tomi', 'O');
   let counter = 0;
+  let winner;
 
   Gameboard.boardContainer.onclick = (event) => {
     let target = event.target;
@@ -43,26 +48,66 @@ const displayController = (function() {
 
     if (cell === '') {
       if (counter % 2 === 0) {
-        Gameboard.board[index] = gabi.getSign();
-        counter++;
+        drawBoard(player1, index);
       } else {
-        Gameboard.board[index] = tomi.getSign();
-        counter++;
+        drawBoard(player2, index);
       }
-    }
 
+      if (counter <= 7) {
+        counter++;
+      } else if (winner === undefined) {
+        isTie();
+      } 
+    }
     Gameboard.clearBoard();
     Gameboard.render();
+    if (winner !== undefined) {
+      console.log(winner);
+    }
   }
+
+  const drawBoard = (player, index) => {
+    let sign = player.getSign();
+    Gameboard.board[index] = sign;
+    player.board.splice(index, 1, sign);
+    if (counter >= 4) {
+      winner = checkPlayer(player, sign);
+    }
+  }
+
+  const checkPlayer = (player, sign) => {
+    for (let pattern of winningPattern) {
+      let patternToMatch = [sign, sign, sign];
+      let playersPattern = [];
+      for (let element of pattern) {
+        playersPattern.push(player.board[element]);
+      }
+      if (patternToMatch.toString() === playersPattern.toString()) {
+        return player.getName();
+      }
+    }
+  }
+
+  const declareWinner = (player) => {
+    Gameboard.clearBoard();
+    Gameboard.render();
+    console.log(`Winner is: ${player.getName()}`);
+  } 
+
+  const isTie = () => {
+    console.log("It's a tie!");
+  }
+
+
+  const winningPattern = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8],
+    [0, 3, 6], [1, 4, 7], [2, 5, 8],
+    [0, 4, 8], [2, 4, 6]
+  ];
+
 })();
 
-const Player = (name, sign) => {
-  const getSign = () => sign;
-  const getName = () => name;
 
-  return {getSign, getName};
-}
-  
 
-const gabi = Player('Gabi', 'X');
-const tomi = Player('Tomi', 'O');
+
+
