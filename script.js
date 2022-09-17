8040c487c7302be0b4a722c6a8e7685f9eb882fb
Gabi <pagameri@gmail.com>
2022-09-17 23:42:54 +0100

    Finalise first draft
    Finished the first full version of the game.
    
    Currently it is possible to play against the computer only in theory.
    Will tackle adding AI to it so the game is set up to accommodate for it.
    
    Created 3 modules and 1 factory, limiting global variables as much
    as possible.

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const Gameboard = (() => {
   const boardContainer = document.querySelector('.board-container');
   const cells = document.querySelectorAll('.cell');
   
-  const board = ['', '', '', '', '', '', '', '', ''];
+  let board = ['', '', '', '', '', '', '', '', ''];
   
   const render = () => {
     let index = 0;
@@ -12,57 +12,90 @@ const Gameboard = (() => {
     });
   }
 
-  const clearBoard = () => {
+  const emptyCells = () => {
     cells.forEach((cell) => {
       cell.innerText = '';
     });
   }
 
-  return { render, clearBoard, boardContainer, board }
+  return {render, emptyCells, boardContainer, board}
 
 })();
 
 Gameboard.render();
 
 
+const Player = (name, sign) => {
+  const getSign = () => sign;
+  const getName = () => name;
+  let board = ['', '', '', '', '', '', '', '', ''];
+
+  return {getSign, getName, board};
+}
+
 
 const displayController = (() => {
-  const Player = (name, sign) => {
-    const getSign = () => sign;
-    const getName = () => name;
-    let board = ['', '', '', '', '', '', '', '', ''];
-  
-    return {getSign, getName, board};
-  }
-  
-  const player1 = Player('Gabi', 'X');
-  const player2 = Player('Tomi', 'O');
   let counter = 0;
-  let winner;
+  let winner, player1, player2;
+  const onePlayer = document.querySelector('#sg-btn1');
+  const twoPlayer = document.querySelector('#sg-btn2');
 
-  Gameboard.boardContainer.onclick = (event) => {
-    let target = event.target;
-    let index = target.getAttribute('id');
-    let cell = Gameboard.board[index];
+  onePlayer.addEventListener('click', e => {
+    e.preventDefault();
+    resetGame();
+    player1 = Player(document. querySelector('#player').value, 'X');
+    player2 = Player('Computer', 'O');
+    modalController.toggleModal1();
+    playGame();
+  });
+
+  twoPlayer.addEventListener('click', e => {
+    e.preventDefault();
+    resetGame();
+    player1 = Player(document. querySelector('#player1').value, 'X');
+    player2 = Player(document. querySelector('#player2').value, 'O');
+    modalController.toggleModal2();
+    playGame();
+  });
+
+  const resetGame = () => {
+    for (let i = 0; i < 9; i++) {
+      Gameboard.board[i] = '';
+    }
+    Gameboard.emptyCells();
+    winner = undefined;
+    counter = 0;
+  }
+
+  const playGame = () => {
+    Gameboard.boardContainer.onclick = (event) => {
+      let target = event.target;
+      let index = target.getAttribute('id');
+      let cell = Gameboard.board[index];
 
 
-    if (cell === '') {
-      if (counter % 2 === 0) {
-        drawBoard(player1, index);
-      } else {
-        drawBoard(player2, index);
+      if (cell === '') {
+        if (counter % 2 === 0) {
+          drawBoard(player1, index);
+        } else {
+          drawBoard(player2, index);
+        }
+
+        if (counter <= 7) {
+          counter++;
+        } else if (winner === undefined) {
+          isTie();
+        } 
       }
 
-      if (counter <= 7) {
-        counter++;
-      } else if (winner === undefined) {
-        isTie();
-      } 
-    }
-    Gameboard.clearBoard();
-    Gameboard.render();
-    if (winner !== undefined) {
-      console.log(winner);
+      Gameboard.emptyCells();
+      Gameboard.render();
+
+      if (winner !== undefined) {
+        let opponent = player2.getName();
+        modalController.toggleMessageModal(winner, opponent);
+        Gameboard.boardContainer.onclick = '';
+      }
     }
   }
 
@@ -88,16 +121,10 @@ const displayController = (() => {
     }
   }
 
-  const declareWinner = (player) => {
-    Gameboard.clearBoard();
-    Gameboard.render();
-    console.log(`Winner is: ${player.getName()}`);
-  } 
-
   const isTie = () => {
-    console.log("It's a tie!");
+    modalController.toggleTie();
+    Gameboard.boardContainer.onclick = '';
   }
-
 
   const winningPattern = [
     [0, 1, 2], [3, 4, 5], [6, 7, 8],
@@ -108,6 +135,69 @@ const displayController = (() => {
 })();
 
 
+const modalController = (() => {
+  const modal1 = document.querySelector('.modal-1');
+  const modal2 = document.querySelector('.modal-2');
+  const messageModal = document.querySelector('.message-modal');
+  const trigger1 = document.querySelector('.trigger-1');
+  const trigger2 = document.querySelector('.trigger-2');
+  const closeButton1 = document.querySelector('.close-button-1');
+  const closeButton2 = document.querySelector('.close-button-2');
+  const closeButton3 = document.querySelector('.close-button-3');
+  const messageHead = document.querySelector('.message-head');
+  const message = document.querySelector('.modal-content p');
+  
+  function toggleModal1() {
+    modal1.classList.toggle('show-modal-1');
+  }
+  
+  function toggleModal2() {
+    modal2.classList.toggle('show-modal-2');
+  }
+  
+  function toggleMessageModal(winner, player2) {
+    messageModal.classList.toggle('show-message-modal');
+    if (player2 === 'Computer') {
+      if (player2 === winner) {
+        messageHead.innerText = 'Game over!';
+        message.innerText = 'You lose';
+      } else {
+        messageHead.innerText = 'Congratulations!';
+        message.innerText = 'You win';
+      }
+    } else {
+      messageHead.innerText = 'Congratulations!';
+      message.innerText = `${winner} wins`;
+    }
+  }
 
+  function toggleTie() {
+    messageModal.classList.toggle('show-message-modal');
+    messageHead.innerText = 'It\'s a tie';
+    message.innerText = 'Play again';
+  }
+  
+  function closeMessageModal() {
+    messageModal.classList.toggle('show-message-modal');
+  }
 
+  function windowOnClick(event) {
+    if (event.target === modal1) {
+      toggleModal1();
+    } else if (event.target === modal2) {
+      toggleModal2();
+    } else if (event.target === messageModal) {
+      toggleMessageModal();
+    }
+  }
+  
+  trigger1.addEventListener('click', toggleModal1);
+  trigger2.addEventListener('click', toggleModal2);
+  closeButton1.addEventListener('click', toggleModal1);
+  closeButton2.addEventListener('click', toggleModal2);
+  closeButton3.addEventListener('click', closeMessageModal);
+  window.addEventListener('click', windowOnClick);
 
+  return {toggleModal1, toggleModal2, toggleMessageModal, toggleTie};
+
+})();
